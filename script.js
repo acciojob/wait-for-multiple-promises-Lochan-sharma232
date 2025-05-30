7f8363391dbcd1d018229d5fb380de43799f5908
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-30 20:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your JS code here. If required.
+const table=document.querySelector("#output tbody");
+table.innerHTML="";
+
 const promise1=()=>{
 	return new Promise1=((resolve,reject)=>{
 		setTimeOut(()=>{
 			resolve("promise1")
 		}
-	},1500);
+	},2000);
 })
 }
