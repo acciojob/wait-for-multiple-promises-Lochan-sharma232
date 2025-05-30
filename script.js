9f678a5c3521b9b7c26a1bce469c4785b277b49a
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-30 21:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-//your JS code here. If required.
 const table=document.querySelector("#output tbody");
 // table.innerHTML="";
 
-const Promise 1=()=>{
+const Promise1=()=>{
 	return new Promise((resolve,reject)=>{
-		setTimeOut(()=>{
+		setTimeout(()=>{
 			resolve("promise1");
 	},2000);
 })
 }
-const Promise 2=()=>{
+const Promise2=()=>{
 	return new Promise((resolve,reject)=>{
-		setTimeOut(()=>{
+		setTimeout(()=>{
 			resolve("promise2");
 	},1000);
 })
 }
-const Promise 3=()=>{
+const Promise3=()=>{
 	return new Promise((resolve,reject)=>{
-		setTimeOut(()=>{
+		setTimeout(()=>{
 			resolve("promise3");
 	},1500);
 })
 }
-Promise.all[(promise1(),promise2(),promise3())].then((data)=>{
+Promise.all([Promise1(),Promise2(),Promise3()]).then((data)=>{
 	console.log(data)
 })
