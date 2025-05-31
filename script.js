1639a1a4b6f2915b9e5f1f82100bdb61e0ab1fa9
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-31 14:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-// table.innerHTML="";
+
+const data=performance.now();
 const table=document.getElementById("output");
 
 const Promise1=()=>{
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			resolve("promise1");
-	},2000);
+	},1000);
 })
 }
 const Promise2=()=>{
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			resolve("promise2");
-	},1000);
+	},2000);
 })
 }
 const Promise3=()=>{
 	return new Promise((resolve,reject)=>{
 		setTimeout(()=>{
 			resolve("promise3");
-	},1500);
+	},2000);
 })
 }
 
-Promise.all([Promise1(),Promise2(),Promise3()]).then((data)=>{
-	console.log(data)
+Promise.all([promise1(),promise2(),promise3()]).then((results)=>{
+ 
+    const endTime=performance.now()
+    const totalTime=((endTime-startTime)/1000).toFixed(3)
+ 
+   const tbody=document.getElementById('output')
+ 
+   tbody.innerHTML=""
+ 
+   const row=document.createElement("tr")
+ 
+   results.forEach(result => {
+    const row=document.createElement("tr")
+ 
+    row.innerHTML=`
+    <td>${result.label}</td>
+    <td>${result.time.toFixed(3)
+    }</td>
+    `
+    tbody.appendChild(row)
+   });
+ 
+   const totalRow=document.createElement("tr")
+   totalRow.innerHTML=`
+   <td>Total</td>
+   <td>${totalTime}</td>
+   `
+ 
+   tbody.appendChild(totalRow)
+ 
 })
+ 
+ 
